e3872629548996ecef91b3b219fe74929885f72d
fujia <fujia.site@outlook.com>
2023-02-26 15:36:49 +0800

    feat: add new icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,37 @@
     windows_subsystem = "windows"
 )]
 
+struct Database;
+
+#[derive(serde::Serialize)]
+struct CustomResponse {
+    message: String,
+    other_val: usize,
+}
+
+async fn some_other_function() -> Option<String> {
+    Some("response".into())
+}
+
+#[tauri::command]
+async fn my_custom_command(
+    window: tauri::Window,
+    number: usize,
+    database: tauri::State<'_, Database>,
+) -> Result<CustomResponse, String> {
+    println!("Called form {}", window.label());
+    let result: Option<String> = some_other_function().await;
+
+    if let Some(message) = result {
+        Ok(CustomResponse {
+            message,
+            other_val: 42 + number,
+        })
+    } else {
+        Err("No result".into())
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -15,7 +46,8 @@ fn say_hi(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, say_hi])
+        .manage(Database {})
+        .invoke_handler(tauri::generate_handler![greet, say_hi, my_custom_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
